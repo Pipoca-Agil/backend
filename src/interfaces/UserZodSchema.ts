17dbac60f2fa8398d9d8351b6a67e69c624762bb
Isaac Almeida <isaacalmeidafilho@gmail.com>
2023-06-15 10:42:07 -0300

    modifica mensagens de erro para pt-br

--- src/interfaces/UserZodSchema.ts
+++ src/interfaces/UserZodSchema.ts
@@ -2,36 +2,36 @@ import { z } from 'zod';
 
 const UserZodSchema = z.object({
   name: z.string({
-    required_error: 'Name is required',
-    invalid_type_error: 'Name must be a string',
+    required_error: 'O nome é obrigatório',
+    invalid_type_error: 'O nome deve ser uma string',
   })
-    .min(3, { message: 'Name must be 3 or more characters long' }),
+    .min(3, { message: 'O nome deve ter no mínimo 3 caracteres' }),
 
   surname: z.string({
-    required_error: 'Surname is required',
-    invalid_type_error: 'Surname must be a string',
+    required_error: 'O sobrenome é obrigatório',
+    invalid_type_error: 'O sobrenome deve ser uma string',
   })
-    .min(3, { message: 'Surname must be 3 or more characters long' })
+    .min(3, { message: 'O sobrenome deve ter no mínimo 3 caracteres' })
     .optional(),
 
   email: z.string({
-    required_error: 'Email is required',
-    invalid_type_error: 'Email must be a string',
+    required_error: 'Email é obrigatório',
+    invalid_type_error: 'Email deve ser uma string',
   })
-    .email({ message: 'Invalid email address' }),
+    .email({ message: 'Endereço de Email inválido' }),
 
   password: z.string({
-    required_error: 'Password is required',
-    invalid_type_error: 'Password must be a string',
+    required_error: 'Senha é obrigatório',
+    invalid_type_error: 'Senha deve ser uma string',
   })
-    .min(8, { message: 'Password must be 8 or more characters long' })
-    .regex(/[a-z]/, { message: 'Password must contain at least one lowercase letter' })
-    .regex(/[A-Z]/, { message: 'Password must contain at least one uppercase letter' })
-    .regex(/[0-9]/, { message: 'Password must contain at least one digit' }),
+    .min(8, { message: 'Senha deve ter 8 caracteres ou mais' })
+    .regex(/[a-z]/, { message: 'A senha deve conter pelo menos uma letra minúscula' })
+    .regex(/[A-Z]/, { message: 'A senha deve conter pelo menos uma letra maiúscula' })
+    .regex(/[0-9]/, { message: 'A senha deve conter pelo menos um número' }),
 
   role: z.string({
-    required_error: 'Role is required',
-    invalid_type_error: 'Role must be a string',
+    required_error: 'A função é obrigatória',
+    invalid_type_error: 'A função deve ser uma string',
   })
     .default('user')
     .optional(),
