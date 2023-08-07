e1e8787ca0715c8475e3b0336c4808215ee1e62f
PedroBrandao05 <pedro-brandao2012@hotmail.com>
2023-08-07 18:55:21 -0300

    refactor: status code for login

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -30,7 +30,7 @@ export default class UserController {
     const { email, password } = req.body;
     try {
       const token = await this.userModel.signin({ email, password });
-      return res.status(201).json({ token });
+      return res.status(200).json({ token });
     } catch (err) {
       return res.status(500).json({ err });
     }
